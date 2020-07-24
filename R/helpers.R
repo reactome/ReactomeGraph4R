29e46583560201f90ca441bd5f6a4367cce05176
chilampoon <clpoon807@gmail.com>
2020-07-24 14:51:14 +0800

    Function updates & README

--- R/helpers.R
+++ R/helpers.R
@@ -5,33 +5,34 @@
   # connect and store the variable to namespace
   con <- .login()
   options(con = con)
-  #assign("con", con, envir = asNamespace(reactome.graphdb4r)) ##thinking...
   
   if (con$ping() != 200) {
     # an error msg has been printed in the above line `con$ping()`
-    stop("Try to check your database and user info?")
+    stop("FYI - tutorials for graph db: https://reactome.org/dev/graph-database")
   } else {
-    packageStartupMessage("Connection built, welcome to Reactome Graph Database!") 
+    dbi <- call_neo4j('MATCH (dbi:DBInfo) RETURN dbi.version', con) # get version
+    packageStartupMessage(paste0("Connection built, welcome to Reactome Graph Database v", dbi[["dbi.version"]]$value, "!"))
   }
 }
 
 
 # log into local neo4j server
 .login <- function() {
-  url <- "http://localhost:7474"
-  while (!utils::askYesNo(paste0("Is the url '", url, "'?"))) {
-    new.port <- readline(prompt="specify port: ")
-    url <- paste0("http://localhost:", new.port)
+  # specify port
+  port <- 7474
+  while (TRUE) {
+    url <- paste0("http://localhost:", port)
+    ans <- utils::askYesNo(paste0("Is the url '", url, "'?"))
     
-    new.ans <- utils::askYesNo(paste0("Is the url ", url, "?"))
-    if (new.ans) break
-    if (is.na(new.ans)) stop("Cancel")
+    if (ans) break
+    if (is.na(ans)) stop("Cancel")
+    port <- readline(prompt="specify port: ")
   }
   
   # get user & pwd if NEO4J_AUTH is not none
-  if (utils::askYesNo("Does NeoJ require authentication?")) {
+  if (utils::askYesNo("Does Neo4J require authentication?")) {
     user <- readline(prompt="Username: ")
-    password <- readline(prompt="Password: ")
+    suppressMessages(password <- getPass::getPass("Password: ")) # prevent warnings in R CHECK
   } else {
     user <- "neo4j"
     password <- "neo4j"
@@ -48,16 +49,18 @@
   # ensure correct input
   output <- match.arg(output, several.ok = TRUE)
   species <- as.character(species)
+  
+  # use the connexion object locally
   con <- getOption("con")
   
   # get all species info
   query <- 'MATCH (s:Species) RETURN s'
-  all.species <- call_neo4j(query, con)
+  all.species <- neo4r::call_neo4j(query, con)
   all.species <- all.species[['s']]
   
   # to see what data type this species arg is by checking which column it belongs to
   species.data.type <- colnames(all.species)[apply(all.species, 2, function(col) species %in% unlist(col))]
-  if (length(species.data.type) == 0) stop("Please input a species listed in Reactome")
+  if (length(species.data.type) == 0) stop(paste0(species, " not listed in Reactome"))
 
   # output
   species.data.type <- species.data.type[1] # in case type==c("displayName","name")
@@ -75,10 +78,10 @@
 # generate the ID term in WHERE lines (for stId & dbId)
 .genIdTerm <- function(id) {
   id <- toupper(id)
-  if (grepl("^R-...-", id)) {
-    paste0('.stId = ', '"', id, '"')
+  if (grepl("^R-[A-Z]{3}-", id)) {
+    paste0('.stId = "', id, '"')
   } else if (grepl("^[0-9]+$", id)) {
-    paste0('.dbId = ', id)
+    paste0(".dbId = ", id)
   } else {
     stop("Is this id correct?")
   }
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# reactome.graphdb4r
+# R interface for Reactome Graph Database
+
+<!-- badges: start -->
+[![Project Status: WIP – Initial development is in progress, but there has not yet been a stable, usable release suitable for the public.](https://www.repostatus.org/badges/latest/wip.svg)](https://www.repostatus.org/#wip)
+[![R build status](https://github.com/reactome/reactome.graphdb4r/workflows/R-CMD-check/badge.svg)](https://github.com/reactome/reactome.graphdb4r/actions)
+<!-- badges: end -->
+
+
+## Local Reactome Graph Database setup
+
+1. Get Docker: https://docs.docker.com/get-docker/
+2. Follow instructions [here](https://github.com/reactome/BCC-2020/tree/master/graph-database)
+
+## Installation
+
+Install from GitHub:
+```
+devtools::install_github("reactome/reactome.graphdb4r")
+```
+or from CRAN:
+```
+install.packages("reactome/reactome.graphdb4r")
+```
+
+Note that package `neo4r` __(>= 0.1.3)__ is required, install the lastest one from GitHub:
+```
+devtools::install_github("neo4j-rstats/neo4r")
+```
+
+## Documentation
+
+
+## Feedback
+
+We'd love to hear your feedback! Feel free to open an [issue](https://github.com/reactome/reactome4r/issues) on GitHub.
+
+
