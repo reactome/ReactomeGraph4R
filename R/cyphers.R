48107535beebc56e9e3539356b39399f1ece4786
chilampoon <clpoon807@gmail.com>
2020-08-07 00:26:17 +0800

    Add more helper functions and some queries of the working list

---
+++ R/cyphers.R
@@ -0,0 +1,106 @@
+## for cypher queries
+
+# clauses (connected with ';')
+
+.MATCH <- function(clause.list) {
+  num.clause <- length(clause.list)
+  final.clause <- ""
+  for (num in 1:num.clause) {
+    clause <- as.character(clause.list[[num]])
+    clause <- paste0("MATCH p", num, " = ", clause) # no need to worry about keys in list!
+    final.clause <- ifelse(num == 1, clause, paste(final.clause, clause, collapse = ";"))
+  }
+  final.clause
+}
+
+
+# add id, species, name, etc 
+.WHERE <- function(node, ...) {
+  # make a start
+  clause <- "WHERE "
+  
+  # list(`property` = arg)
+  filters <- list(...)
+  filters <- filters[!sapply(filters, is.null)] # remove NULL elements
+  if ("databaseName" %in% names(filters)) {
+    # can't fetch data if adding Reactome as databaseName so just remove it
+    if (filters[["databaseName"]] == "Reactome") filters <- filters[names(filters) != "databaseName"]
+  }
+
+  for (filter in names(filters)) {
+    .checkInfo(filter, "property")
+    if (filter == "id") {
+      db <- ifelse("databaseName" %in% names(filters), filters[["databaseName"]], "Reactome") 
+      add <- paste0(node, .genIdTerm(filters[[filter]], database = db))
+    } else if (filter == "speciesName") {
+      add <- paste0(node, '.speciesName = "', .matchSpecies(filters[[filter]], "displayName"), '"')
+    } else {
+      # add dquotes for those include alphabet
+      tmp <- ifelse(grepl("^[0-9]+$", filters[[filter]]), filters[[filter]], paste0('"', filters[[filter]], '"'))
+      add <- paste0(node, ".", filter, " = ", tmp)
+    }
+    clause <- ifelse(!grepl(node, clause), paste0(clause, add), paste0(clause, " AND ", add))
+  }
+  clause
+}
+
+
+# graph object needs 'relationships'
+.RETURN <- function(node, type, numOfMatch=1) {
+  clause <- 'RETURN '
+  nodes <- paste(node, collapse = ",")
+  clause <- paste0(clause, nodes)
+  # add `relationships()` for each path
+  if (type == "graph") {
+    numOfMatch <- seq(1, numOfMatch) # vectorize
+    rels <- paste0("relationships(p", numOfMatch, ")")
+    rels <- paste(rels, collapse = ",")
+    clause <- paste0(clause, ",", rels)
+  }
+  clause
+}
+
+
+# variable length relationships
+.varLen <- function(clause, rel, depth, all) {
+  # have a check!
+  .checkInfo(rel, "relationship")
+  if (depth < 1) depth <- 1
+  
+  # depth = 1 by default
+  if (!all && depth == 1) {
+    message("Retrieving immediate connected instances... Specify depth-related arguments for more depths")
+  }
+  
+  # replace
+  new.rel <- ifelse(all, paste0(rel, "*"), ifelse(depth > 1, paste0(rel, "*1..", as.integer(depth)), rel))
+  new.clause <- gsub(rel, new.rel, clause)
+  new.clause
+}
+
+
+# generate the ID term - stId, dbId, external id
+.genIdTerm <- function(id, database="Reactome") {
+  id <- gsub("\\s", "", id) # remove blanks
+  if (database == "Reactome") {
+    id <- toupper(id)
+    if (grepl("^R-[A-Z]{3}-", id)) {
+      term <- paste0('.stId = "', id, '"')
+    } else if (grepl("^[0-9]+$", id)) {
+      term <- paste0(".dbId = ", id)
+    } else {
+      stop("Is this id correct?", call.=FALSE)
+    }
+  } else { # non-Reactome ids
+    if (grepl("^[0-9]+$", id)) {
+      term <- paste0(".identifier = ", id)
+    } else {
+      # add quotes
+      term <- paste0('.identifier = "', id, '"')
+    }
+  }
+  term
+}
+
+
+
--- R/helpers.R
+++ R/helpers.R
@@ -8,9 +8,9 @@
   
   if (con$ping() != 200) {
     # an error msg has been printed in the above line `con$ping()`
-    stop("FYI - tutorials for graph db: https://reactome.org/dev/graph-database")
+    stop("FYI - tutorials for graph db: https://reactome.org/dev/graph-database", call.=FALSE)
   } else {
-    dbi <- call_neo4j('MATCH (dbi:DBInfo) RETURN dbi.version', con) # get version
+    dbi <- neo4r::call_neo4j('MATCH (dbi:DBInfo) RETURN dbi.version', con) # get version
     packageStartupMessage(paste0("Connection built, welcome to Reactome Graph Database v", dbi[["dbi.version"]]$value, "!"))
   }
 }
@@ -25,7 +25,7 @@
     ans <- utils::askYesNo(paste0("Is the url '", url, "'?"))
     
     if (ans) break
-    if (is.na(ans)) stop("Cancel")
+    if (is.na(ans)) stop("Cancel", call.=FALSE)
     port <- readline(prompt="specify port: ")
   }
   
@@ -60,7 +60,7 @@
   
   # to see what data type this species arg is by checking which column it belongs to
   species.data.type <- colnames(all.species)[apply(all.species, 2, function(col) species %in% unlist(col))]
-  if (length(species.data.type) == 0) stop(paste0(species, " not listed in Reactome"))
+  if (length(species.data.type) == 0) stop(paste0(sQuote(species), " not listed in Reactome"))
 
   # output
   species.data.type <- species.data.type[1] # in case type==c("displayName","name")
@@ -75,23 +75,102 @@
 }
 
 
-# generate the ID term in WHERE lines (for stId & dbId)
-.genIdTerm <- function(id) {
-  id <- toupper(id)
-  if (grepl("^R-[A-Z]{3}-", id)) {
-    paste0('.stId = "', id, '"')
-  } else if (grepl("^[0-9]+$", id)) {
-    paste0(".dbId = ", id)
-  } else {
-    stop("Is this id correct?")
+# check if value(s) in db or not (mostly used in internal checks)
+.checkInfo <- function(info, type=c("label", "relationship", "property")) {
+  type <- match.arg(type, several.ok = FALSE)
+  info <- gsub("^:", "", info)
+  
+  con <- getOption("con") # get connexion
+  if (type == "label") {
+    info <- strsplit(info, split=':', fixed=TRUE)[[1]]
+    terms <- con$get_labels()
+  } else if (type == "relationship") {
+    info <- strsplit(info, split='|', fixed=TRUE)[[1]]
+    terms <- con$get_relationships()
+  } else if (type == "property") {
+    terms <- con$get_property_keys()
+  }
+  terms <- terms$labels # to character
+  
+  throw <- info[!info %in% terms]
+  throw <- throw[throw != "id"] # exclude 'id' which represents 'dbId' & 'stId' & 'identifier'
+  if (length(throw) > 0) {
+    throw <- paste(throw, collapse = ", ")
+    warning(paste0("The ", type, " '", throw, "' is not in this database"), call.=FALSE) ## or stop()? ##
   }
 }
 
 
+# call neo4j API
+.callAPI <- function(query, type, verbose=FALSE, msg=NULL, ...) {
+  # being wordy if 'type' arg missing
+  if (verbose) message("Type argument not specified, retrieving 'row' data... For graph data, specify type='graph'")
+
+  # get the connexion object locally
+  con <- getOption("con")
+  
+  # call API
+  res <- neo4r::call_neo4j(query = query,
+                           con = con,
+                           type = type,
+                           ...)
+  # check & send error if any
+  .errMsg(res, msg=msg)
+  
+  # manipulate 'row' content
+  if (type == "row")  res <- lapply(res, function(tbl) unique(as.data.frame(tbl)))
+  res
+}
 
 
+# error messages
+.errMsg <- function(res, msg=NULL) {
+  # send more information
+  if (length(res) == 0) {
+    # customized info following 'No data returned'
+    if(!is.null(msg)) stop(msg, call.=FALSE)
+  } else if ("error_message" %in% names(res)) {
+    # query syntax error
+    stop(paste0(res[["error_code"]], "\n", res[["error_message"]]), call.=FALSE)
+  }
+}
 
-## write Cypher
+
+# get value of specific attribute(s) with a given id/name
+.getSlotValue <- function(dbo, type=c("id", "name"), resource="Reactome", slot) {
+  # assign value
+  id <- NULL -> name
+  if (type == "id") {
+    id <- dbo
+  } else {
+    name <- dbo
+  }
+  
+  # check info
+  .checkInfo(slot, "property")
+  
+  # retrieve
+  c.MATCH <- .MATCH(list('(dbo:DatabaseObject)'))
+  c.WHERE <- .WHERE("dbo", id=id, displayName=name)
+  c.RETURN <- .RETURN(paste0("dbo.", slot), type="row") # can input >1 slots!
+  query <- paste(c.MATCH, c.WHERE, c.RETURN, collapse = ";")
+  .callAPI(query, type="row")
+}
+
+
+# get all labels/keys of node(s)
+# more types to be added
+.getNodeInfo <- function(node.where, type=c("keys", "labels")) {
+  type <- match.arg(type)
+  query <- paste('MATCH (dbo:DatabaseObject)',
+                 node.where,
+                 paste0('UNWIND ', type, '(dbo) AS info'),
+                 'RETURN distinct(info)',
+                 collapse = ";")
+  res <- .callAPI(query, type="row")
+  res[["info"]]
+}
+
 
 
 
