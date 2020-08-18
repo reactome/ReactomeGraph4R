0cb238c28fa423439c812945136d78306aa7faeb
chilampoon <clpoon807@gmail.com>
2020-08-18 13:51:59 +0800

    Change back to neo4r data-parsing method for graph data

--- R/data.parsing.R
+++ R/data.parsing.R
@@ -5,7 +5,8 @@
 # Note: neo4r 0.1.3 doesn't return syntax errors of cypher queries when format='json'
 
 # not include stats & meta
-.parseJSON <- function(json.res, return.names, type, msg=NULL) {
+.parseJSON <- function(json.res, type, return.names, msg=NULL) {
+
   # transform into list
   json.list <- jsonlite::fromJSON(json.res)
   
@@ -18,14 +19,7 @@
   # Turn NULL to NA & get the data
   res.data <- .null_to_na(json.results)
   
-  # Get the name of the return elements
-  res.names <- return.names
-  
-  if (type == "row") {
-    .parse_row(res.data, res.names)
-  } else if (type == "graph") {
-    .parse_graph(res.data)
-  }
+  .parse_row(res.data, return.names)
 }
 
 
@@ -84,14 +78,31 @@
                       x
                      })
   # add names
-  if (!is.null(res.names)) {
-    # eg: .getSlotValue(dbObject="RCOR1 [nucleoplasm]", dbObject.type="name", slot=c("dbId", "displayName"))
-    if (length(res) == 1 && class(res[[1]]) == "data.frame") {
-      colnames(res[[1]]) <- res.names # usually the lengths are matched, not check here
-    } else if (length(res.names) != length(res)) {
-      stop("Check inputed return names & RETURN clause") # internal check
+  if (!is.null(res.names) && length(res) > 0) {
+    if (any(grepl("\\.", res.names))) {
+      # there are specified attributes of node(s) in RETURN -- e.g. dbo.dbId, dbo.displayName
+      # eg: matchObject(displayName="RCOR1 [nucleoplasm]", attribute=c("dbId", "displayName"))
+      
+      # get unique node names
+      node.names <- unique(sapply(strsplit(res.names, split="\\."), function(x) x[[1]]))
+      names(res) <- .goodName(node.names)
+      if (length(node.names) != length(res)) {
+        stop("Check inputed return names & RETURN clause")
+      }
+      
+      # name the columns with specified attributes
+      node.names.with.dot <- unique(sapply(strsplit(res.names[grep("\\.", res.names)], split="\\."), function(x) x[[1]]))
+      for (name in node.names.with.dot) {
+        attr.name <- res.names[grep(name, res.names)]
+        attr.name <- gsub(paste0(name, "."), "", attr.name)
+        colnames(res[[.goodName(name)]]) <- attr.name
+      }
     } else {
-      names(res) <- res.names
+      if (length(res.names) != length(res)) {
+        stop("Check inputed return names & RETURN clause")
+      } else {
+        names(res) <- res.names
+      }
     }
   }
   res
@@ -128,57 +139,6 @@
 }
 
 
-# parse 'graph' data
-.parse_graph <- function(res.data) {
-  # Get the elements
-  graph <- as.list(res.data[["graph"]])
-  nodes <- graph[["nodes"]]
-  relations <- graph[["relationships"]]
-  
-  # Verify that there is something to return
-  if (length(nodes) == 0 & length(relations) == 0) {
-    message("No graph data found.")
-    message("Either your call can't be converted to a graph \nor there is no data at all matching your call.")
-    message("Verify your call or try type = \"row\".")
-  }
-  
-  # Do something only if there are nodes
-  if (length(nodes) != 0) {
-    res.nodes <- .operate_list(nodes)
-  }
-  
-  # Do something only if there are relations
-  if (length(relations) != 0) {
-    res.relations <- .operate_list(relations)
-  }
-  
-  # combine nodes & relationships
-  res <- purrr::compact(
-    list(
-      nodes = res.nodes,
-      relationships = res.relations
-    )
-  )
-  res
-}
-
-# handle nodes & relationships data
-.operate_list <- function(list) {
-  ## not sure if there are other situations ##
-  
-  # select the one having maximum number of nodes/relationships
-  max.one <- list[which.max(sapply(list, function(x) nrow(x)))]
-  max.one <- as.list(max.one[[1]])
-  
-  # turn dataframe into separate list
-  max.one[["properties"]] <- apply(max.one[["properties"]], 1, as.list)
-  
-  # turn final results into a dataframe
-  res.list <- as.data.frame(do.call(cbind, max.one))
-  
-  # sort by id
-  res.list$id <- as.integer(res.list$id)
-  res.list[order(res.list$id), ]
-}
+### include relationships data
 
 
